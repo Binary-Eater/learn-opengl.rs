c7c9338fc097fc9b9bd3024a92a489320acd0ee6
Rahul Rameshbabu <sergeantsagara@protonmail.com>
2026-06-18 16:00:30 -0700

    Complete Learn OpenGL Hello Window chapter in Rust
    Prepare the needed infrastructure for GLFW and GL API calls in Rust. Use the
    infrastructure to complete Hello Window.
    
    Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+extern crate glfw;
+
+use glfw::{Context, Window};
+
+fn framebuffer_size_callback(_window: &mut Window, width: i32, height: i32) {
+    // TODO figure out why this is safe and write a SAFETY comment
+    // TIL it's because the functions are dynamically loaded via the symbol lookup...
+    unsafe {
+        gl::Viewport(0, 0, width, height);
+    }
+}
+
+fn process_input(window: &mut Window) {
+    match window.get_key(glfw::Key::Escape) {
+        glfw::Action::Press => window.set_should_close(true),
+        _ => (),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    use glfw::fail_on_errors;
+
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+
+    glfw.window_hint(glfw::WindowHint::ContextVersionMajor(3));
+    glfw.window_hint(glfw::WindowHint::ContextVersionMinor(3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+
+    let (mut window, _events) = glfw
+        .create_window(800, 600, "Hello this is window", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    /* Implement symbol lookup closure for GL functions */
+    gl::load_with(|s| window.get_proc_address(s) as *const _);
+
+    window.make_current(); /* Equivalent to glfwMakeContextCurrent */
+    window.set_framebuffer_size_callback(framebuffer_size_callback);
+
+    // render loop
+    /* Equivalent of glfwWindowShouldClose */
+    while !window.should_close() {
+        // input
+        process_input(&mut window);
+
+        // rendering commands here
+        // TODO write SAFETY comment
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        // check and call events and swap the buffers
+        window.swap_buffers();
+        glfw.poll_events();
+    }
+
+    /* There is no glfwTerminate call due to Drop trait */
 }
