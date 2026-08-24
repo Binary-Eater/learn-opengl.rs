5d75001d8ea9da2a43af16460bd84ef8a3baae22
Rahul Rameshbabu <sergeantsagara@protonmail.com>
2026-08-23 19:37:31 -0700

    Finished the entire Learn OpenGL Hello Triangle chapter
    Draw the rectangle as expected by the end of the chapter using element buffer
    objects.
    
    Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -40,19 +40,44 @@ fn main() -> Result<(), String> {
     window.make_current(); /* Equivalent to glfwMakeContextCurrent */
     window.set_framebuffer_size_callback(framebuffer_size_callback);
 
+    /*
+     * A world without element buffer objects for drawing a rectangle.
+     *
+     * let vertices: [f32; _] = [
+     *     /*     x,        y,       z, */
+     *     // first triangle
+     *      0.5_f32,  0.5_f32, 0.0_f32,  // top right
+     *      0.5_f32, -0.5_f32, 0.0_f32,  // bottom right
+     *     -0.5_f32,  0.5_f32, 0.0_f32,  // top left
+     *     // second triangle
+     *      0.5_f32, -0.5_f32, 0.0_f32,  // bottom right
+     *     -0.5_f32, -0.5_f32, 0.0_f32,  // bottom left
+     *     -0.5_f32,  0.5_f32, 0.0_f32,  // top left
+     * ];
+     */
+
     let vertices: [f32; _] = [
         /*     x,        y,       z, */
-        -0.5_f32, -0.5_f32, 0.0_f32,
-         0.5_f32, -0.5_f32, 0.0_f32,
-         0.0_f32,  0.5_f32, 0.0_f32,
+        // first triangle
+         0.5_f32,  0.5_f32, 0.0_f32,  // top right
+         0.5_f32, -0.5_f32, 0.0_f32,  // bottom right
+        -0.5_f32, -0.5_f32, 0.0_f32,  // bottom left
+        -0.5_f32,  0.5_f32, 0.0_f32,  // top left
+    ];
+    let indices: [u32; _] = [
+        /* NOTE: indices start from 0 and end at 3 */
+        0, 1, 3,   // first triangle
+        1, 2, 3,   // second triangle
     ];
 
     let mut vbo: u32 = 0;
     let mut vao: u32 = 0;
+    let mut ebo: u32 = 0;
     // SAFETY: Assume the functions are dynamically loaded via the symbol lookup.
     unsafe {
         gl::GenVertexArrays(1, &mut vao);
         gl::GenBuffers(1, &mut vbo);
+        gl::GenBuffers(1, &mut ebo);
 
         gl::BindVertexArray(vao);
 
@@ -71,6 +96,16 @@ fn main() -> Result<(), String> {
              *     .expect("failed to represent usize value as an i32"),
             */
             vertices.as_ptr().cast(), /* cast *const f32 into *const c_void */
+            gl::STATIC_DRAW,
+        );
+
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+        gl::BufferData(
+            gl::ELEMENT_ARRAY_BUFFER,
+            std::mem::size_of_val(&indices)
+                .try_into()
+                .expect("failed to represent usize value as an i32"),
+            indices.as_ptr().cast(), /* cast *const f32 into *const c_void */
             gl::STATIC_DRAW,
         );
 
@@ -242,7 +277,18 @@ fn main() -> Result<(), String> {
             gl::Clear(gl::COLOR_BUFFER_BIT);
             gl::UseProgram(shader_program);
             gl::BindVertexArray(vao);
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            /*
+             * Can use polygon mode to help with debugging using wireframes
+             *
+             * wireframe mode: gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
+             * default:        gl::PolygonMode(gl::FRONT_AND_BACK, gl::FILL);
+             */
+            /*
+             * ebo is bounded to vao since the vao tracks the last EBO object
+             * bound while vao is bound.
+             */
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
+            gl::BindVertexArray(0);
         }
 
         // check and call events and swap the buffers
@@ -254,6 +300,7 @@ fn main() -> Result<(), String> {
     unsafe {
         gl::DeleteVertexArrays(1, &vao);
         gl::DeleteBuffers(1, &vbo);
+        gl::DeleteBuffers(1, &ebo);
         gl::DeleteProgram(shader_program);
     }
 
